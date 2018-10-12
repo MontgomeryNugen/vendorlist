f9de2815cbd64a991e9b676479b5ef085b705405
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-10-11 21:59:34 -0400

    changes

--- js/vendorlist.js
+++ js/vendorlist.js
@@ -18,7 +18,7 @@ function render(vendors) {
         row += "<td>" + vendor.id + "</td>";
         row += "<td>" + vendor.code + "</td>";
         row += "<td>" + vendor.address + ", " + vendor.city + ", " + vendor.state + " " + vendor.zip + "</td>";
-        row += "<td>" + vendor.phone + "</td>";
+        row += "<td>" + vendor.phoneNumber + "</td>";
         row += "<td>" + vendor.email + "</td>";
         row += "</tr>";
 
